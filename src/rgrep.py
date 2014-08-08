9013e8e02655c87dd20ea6a949e7b8eecb4890a5
ambidextrousTx <ravisinha@my.unt.edu>
2014-08-07 23:50:00 -0500

    First passing test

---
+++ src/rgrep.py
@@ -0,0 +1,2 @@
+def rgrep():
+    return 'Usage: python rgrep [options] pattern files\nThe options are the same as grep\n'
--- test/test_rgrep.py
+++ test/test_rgrep.py
@@ -1,8 +1,12 @@
 import sys
-sys.path.append('../src/')
 import unittest
+sys.path.append('../src/')
+from rgrep import *
+
 
 class TestRGrep(unittest.TestCase):
+    def test_grep_without_arguments(self):
+        self.assertEquals(rgrep(), 'Usage: python rgrep [options] pattern files\nThe options are the same as grep\n')
 
 
 if __name__ == '__main__':
