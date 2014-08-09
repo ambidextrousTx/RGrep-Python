19f89c59d25a4151f0fdf4908cce4a3c23086dc1
ambidextrousTx <ravisinha@my.unt.edu>
2014-08-08 23:36:16 -0500

    Add match for pattern against text

--- src/rgrep.py
+++ src/rgrep.py
@@ -1,2 +1,5 @@
-def rgrep():
-    return 'Usage: python rgrep [options] pattern files\nThe options are the same as grep\n'
+def rgrep(pattern='', text=''):
+    if pattern == '' or text == '':
+        return 'Usage: python rgrep [options] pattern files\nThe options are the same as grep\n'
+    else:
+        return True
--- test/test_rgrep.py
+++ test/test_rgrep.py
@@ -8,6 +8,8 @@ class TestRGrep(unittest.TestCase):
     def test_grep_without_arguments(self):
         self.assertEquals(rgrep(), 'Usage: python rgrep [options] pattern files\nThe options are the same as grep\n')
 
+    def test_grep_pattern_text(self):
+        self.assertEquals(rgrep('hello', 'hello'), True)
 
 if __name__ == '__main__':
     unittest.main()
