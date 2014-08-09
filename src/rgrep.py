0607db211304e59065d0dbab42015ff8ed597f8a
ambidextrousTx <ravisinha@my.unt.edu>
2014-08-08 23:37:26 -0500

    Refactor to remove duplication

--- src/rgrep.py
+++ src/rgrep.py
@@ -2,4 +2,4 @@ def rgrep(pattern='', text=''):
     if pattern == '' or text == '':
         return 'Usage: python rgrep [options] pattern files\nThe options are the same as grep\n'
     else:
-        return True
+        return pattern == text
