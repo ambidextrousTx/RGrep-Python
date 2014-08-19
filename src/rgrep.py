9c7b617829a953c8e7a4b377f8da84f8de94c9bf
ambidextrousTx <ravisinha@my.unt.edu>
2014-08-18 23:26:37 -0500

    Refactor the case insensitive option

--- src/rgrep.py
+++ src/rgrep.py
@@ -1,8 +1,13 @@
+def display_usage():
+    return 'Usage: python rgrep [options] pattern files\nThe options are the same as grep\n'
+
+
 def rgrep(pattern='', text='', case=''):
     if pattern == '' or text == '':
-        return 'Usage: python rgrep [options] pattern files\nThe options are the same as grep\n'
+        return display_usage()
     else:
         if case == 'i':
-            return pattern.lower() in text.lower()
-        else:
-            return pattern in text
+            pattern = pattern.lower()
+            text = text.lower()
+
+        return pattern in text
