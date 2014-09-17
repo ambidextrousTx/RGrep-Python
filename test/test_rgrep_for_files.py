1df8922f362c3a825a9a84cb617948fd95689160
ambidextrousTx <ravisinha@my.unt.edu>
2014-09-16 22:14:58 -0500

    Add new test class for testing file functionality

---
+++ test/test_rgrep_for_files.py
@@ -0,0 +1,11 @@
+'''
+Tests focused on the functionality to deal with files
+'''
+
+import sys
+import unittest
+sys.path.append('../src/')
+from rgrep import *
+
+
+class TestRGrepFiles(unittest.TestCase):
